f9333cd73af99fa672e85c1231ff2f61f640216a
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-25 18:14:39 +0700

    Update and rename v1.sh to cewe++.sh
    update

---
+++ cewe++.sh
@@ -0,0 +1,47 @@
+figlet MR_XID  | lolcat
+
+echo                    " [======<>======<>======<>======]"|lolcat
+echo                    " [Author   :  MR_XiD            ]"|lolcat
+echo                    " [Team     : JATIM BLACKHAT     ]"|lolcat
+echo                    " [Note     : Jangan reqode ya   ]"|lolcat
+echo                    " [Thansk to: All member         ]"|lolcat
+echo                    " [thanks to: I T A              ]"|lolcat
+echo                    " [Note2    : Copyright©MR_XID   ]"|lolcat
+echo                    " [======<>======<>======<>======]"|lolcat
+
+lagi=1
+while [ $lagi -lt 7 ];
+do
+echo -e""
+echo -e $b "1.  TEMBAK CEWE${enda}";
+echo -e"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" | lolcat
+echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" | lolcat
+echo    "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+# TEMBAK CEWE
+
+if [ $saf = 1 ] || [ $saf = 1 ]
+then
+echo -e "----------------------"|lolcat
+echo -e $r "1.cara tembak cewe ${endc}";
+echo -e "----------------------"|lolcat
+echo -e "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+#cara tembak cewe
+
+if [ $saf = 1 ] || [ $saf = 1 ]
+then
+echo "CARA MENEMBAK CEWEK YA TONG 😅" | lolcat
+echo "PERTAMA KAMU HARUS PUNYA DUIT"
+echo "KALO KAGA YAUDAH GAGAL DEH 😅😅
+echo "JOMBLO HAPPY AJA TOD 😂
+exit
+
+;;
+
+*) echo "Tod nomor yang elu pilih kaga ada. coky dulu ya"
+esac
+done
+done
